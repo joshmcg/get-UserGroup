c2e112274edcd1d9600a72365f4938e54ed7b00a
joshmcg <43545081+joshmcg@users.noreply.github.com>
2018-09-25 12:07:36 -0500

    Add files via upload

---
+++ Get-UserGroups.ps1
@@ -0,0 +1,16 @@
+#Get-UserGroups v0.1 9-21-2018 jpm
+
+#Test Names
+$userNames = @("jpmcgoron")
+$userGroups = @()
+$groupInfo = @()
+$groupInfo = [PSCustomObject] @{}
+
+ForEach ($name in $userNames) {
+    $strGroups = $null
+    $userGroups = Get-ADPrincipalGroupMembership $name | Select-Object Name
+    $strGroups = ($userGroups.Name | Out-String).Trim()
+    $groupInfo | Add-Member -MemberType NoteProperty -Name $name.ToString() -Value $strGroups
+}
+
+$groupInfo | Format-Table -wrap
